00150523726d28982d2e3a1f1528432aed6811df
Rakshit Agrawal <rakshit_agrawal@outlook.com>
2015-07-21 15:16:40 -0700

    Added table for analysis_type. Added reference to page and analysis_type in analysis

--- models/tables.py
+++ models/tables.py
@@ -1,18 +1,29 @@
 # Tables to be used by the server.
 
+# Table for Wikipedia pages
 db.define_table('wikipages',
-                Field('pageid', unique=True),
-                Field('title'),
-                Field('lang'),
-                Field('last_known_rev'),
+                Field('pageid', unique=True),  # Page ID same as Wikipedia
+                Field('title'),  # Page title from Wikipedia
+                Field('lang'),  # Page language from Wikipedia
+                Field('last_known_rev'),  # Last known revision of the page to the system
                 )
 
+# Table for different analysis
+db.define_table('analysis_type',
+                Field('name'),  # Name of the analysis, e.g., reputation, or authorship.
+                Field('author'),  # Author of analysis
+                Field('description', 'text'),  # Description of analysis
+                Field('reference_location'),  # Reference path for algorithm code or URL call
+                )
+
+# Table to store page-analysis entries.
 db.define_table('analysis',
-                Field('analysis_type'),  # Type of analysis, e.g., reputation, or authorship.
-                Field('pageid'),  # Unique for each type of analysis.
-                Field('last_annotated'),
-                Field('worker_id'),  # Each worker generates a random id, used to sign these. If blank = nobody working on this.
+                Field('analysis_type', 'reference analysis_type'),  # Type of analysis, e.g., reputation, or authorship.
+                Field('pageid', 'reference wikipages'),  # Unique for each type of analysis.
+                Field('last_annotated'),  # Last annotated revision for the analysis-page pair
+                Field('worker_id'),
+                # Each worker generates a random id, used to sign these. If blank = nobody working on this.
                 Field('work_start_date', 'datetime'),  # If too old, blank both this and worker id.
-                Field('priority', 'double'),
+                Field('priority', 'double'),  # Priority of analysis. #TODO
                 Field('status')  # If active, then analysis needs to be done, else no
                 )
